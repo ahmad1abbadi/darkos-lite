14378045d68aee743130bd200a63c6d6954a07dd
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-04-30 01:33:07 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 clear
-echo -e "Updating termux packages list please wait\n"
+echo "Updating termux packages list please wait"
 apt update &>/dev/null
-echo -e "Upgrading termux packages...this might take some time\n"
-yes | apt-get full-upgrade &>/dev/null
-unlink "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
-ln -s "$PREFIX/etc/termux/mirrors/all" "$PREFIX/etc/termux/chosen_mirrors" &>/dev/null
-echo -e "Selecting best termux packages mirror please wait\n"
-TERMUX_APP_PACKAGE_MANAGER=apt pkg --check-mirror update
-echo -e "please allow storage permission\n"
+echo "Upgrading termux packages...this might take some time"
+apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
+echo "please allow storage permission"
 termux-setup-storage
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
-curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/installglibc.py && python3 installglibc.py
+curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/installglibc.py && python3 installglibc.py
 exit
