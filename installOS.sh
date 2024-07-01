4b671e13fdac3adba970f3fe0815df99105531b5
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-07-01 21:00:44 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -5,7 +5,6 @@ apt update &>/dev/null
 echo "Upgrading termux packages...this might take some time"
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
 echo "please allow storage permission"
-termux-setup-storage
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/installglibc.py && python3 installglibc.py
