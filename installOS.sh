bf6bc592705951d0cdee8de70dd539ee94dfd935
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-08-20 20:05:36 +0300

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -4,7 +4,16 @@ echo "Updating termux packages list please wait"
 apt update &>/dev/null
 echo "Upgrading termux packages...this might take some time"
 apt-get -y --with-new-pkgs -o Dpkg::Options::="--force-confdef" upgrade >/dev/null
-echo "please allow storage permission"
+while true; do
+	termux-setup-storage
+	sleep 4
+    if [[ -d ~/storage ]]; then
+        break
+    else
+        echo -e "Storage permission denied"
+    fi
+    sleep 2
+done
 apt install python --no-install-recommends -y &>/dev/null
 echo "be patient"
 curl -o installglibc.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/installglibc.py && python3 installglibc.py
