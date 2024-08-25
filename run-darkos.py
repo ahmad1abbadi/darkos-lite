cdbc586045049025a76d94280843f56c53c545c4
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-08-25 04:29:09 +0300

    Update run-darkos.py

--- run-darkos.py
+++ run-darkos.py
@@ -79,6 +79,7 @@ def start_wine():
     os.system("$PREFIX/glibc/opt/scripts/termux-x11.sh displayResolutionMode:custom &>/dev/null &")
     os.system(f"$PREFIX/glibc/opt/scripts/termux-x11.sh displayResolutionCustom:{res} &>/dev/null &")
     os.system("box64 wine64 explorer /desktop=shell," + res + " $PREFIX/glibc/opt/apps/DARKOS_configuration.exe &>/dev/null &")
+    time.sleep(2)
     os.system("am start -n com.termux.x11/com.termux.x11.MainActivity &>/dev/null")
     os.system("clear")
     os.system("python3 $PREFIX/bin/photo.py")
