bd97f056c5e4c8a2eaf3693f4526d750841ea25e
ahmad1abbadi <156505848+ahmad1abbadi@users.noreply.github.com>
2024-08-26 16:02:41 +0300

    Update update-darkos.py

--- update-darkos.py
+++ update-darkos.py
@@ -1,6 +1,15 @@
 import os, time, shutil, sys, subprocess, urllib.request, urllib.error, fnmatch
-current_version = "0.911"
-url = 'https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/currently%20version.txt'
+
+R = "\033[1;31m"
+G = "\033[1;32m"
+Y = "\033[1;33m"
+B = "\033[1;34m"
+C = "\033[1;36m"
+W = "\033[1;37m"
+BOLD = "\033[1m"
+
+current_version = "0.1"
+url = 'https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/currently%20version.txt'
 def remove():
     folder_path = '/data/data/com.termux/files/home'
     for filename in os.listdir(folder_path):
@@ -13,48 +22,43 @@ os.system("clear")
 os.system("python3 $PREFIX/bin/photo.py")
 time.sleep(2)   
 print("")
-print("Shutdown OS....")
+print(f"{R}[{W}-{R}]{G}{BOLD} Shutdown OS.... {W}")
 print("")
-print("checking 🔎.....")
+print(f"{R}[{W}-{R}]{G}{BOLD} checking 🔎..... {W}")
 time.sleep(1)
 response = urllib.request.urlopen(url)
 latest_version = response.read().decode('utf-8').strip()
 try:
   if latest_version > current_version:
-    print("update available..... updating......📥")
-    os.system("wget -q --show-progress https://github.com/ahmad1abbadi/darkos-lite/releases/download/lite/AZ.tar.xz")
-    os.system("tar -xJf AZ-lite.tar.xz -C $PREFIX/glibc")
-    os.system("wget -q --show-progress https://github.com/ahmad1abbadi/darkos-lite/releases/download/lite/darkos.tar.xz")
-    os.system("tar -xJf darkos.tar.xz -C /sdcard/")
-    os.system("wget -q --show-progress https://github.com/ahmad1abbadi/darkos-lite/releases/download/lite/update.tar.xz")
-    os.system("tar -xJf update.tar.xz")
-    os.system("wget -q --show-progress https://github.com/ahmad1abbadi/darkos-lite/releases/download/lite/files.tar.xz")
-    os.system("tar -xJf files.tar.xz -C /data/data/com.termux/files")
+    print(f"{R}[{W}-{R}]{G}{BOLD} update available..... {C}updating......📥 {W}")
     os.system("rm $PREFIX/bin/darkos.py")
     os.system("rm $PREFIX/bin/update-darkos.py")
     os.system("rm $PREFIX/bin/run-darkos.py")
     os.system("rm $PREFIX/bin/debug-darkos.py")
-    os.system("rm $PREFIX/bin/setting-darkos.py")
     os.system("rm $PREFIX/bin/darkos")
     os.system("wget -O run-darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/run-darkos.py")
     os.system("wget -O darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/darkos.py")
     os.system("wget -O darkos https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/darkos")
     os.system("wget -O debug-darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/debug-darkos.py")
     os.system("wget -O update-darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/update-darkos.py")
+    os.system("wget -O new-update.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/new-update.py")
     os.system("chmod +x darkos")
     os.system("mv update-darkos.py darkos.py run-darkos.py debug-darkos.py darkos $PREFIX/bin/")
+    os.system("python3 new-update.py")
+    time.sleep(2)
     remove()
-    print("update completed 🎉")
-    print("rebooting")
+    os.system("rm new-update.py")
+    print(f"{R}[{W}-{R}]{G}{BOLD} update completed 🎉 {W}")
+    print(f"{G}{BOLD} rebooting.... {W}")
     time.sleep(3)
 except urllib.error.HTTPError as e:
   if e.code == 404:
-    print("no internet connection 😵 rebooting.....")
+    print(f"{R} no internet connection 😵 {C}rebooting..... {W}")
     time.sleep(2)
   else:
     os.system("rm $PREFIX/bin/darkos.py")
-    os.system("wget -O darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos/main/darkos.py")
+    os.system("wget -O darkos.py https://raw.githubusercontent.com/ahmad1abbadi/darkos-lite/main/darkos.py")
     os.system("mv darkos.py")
-    print("no update available ")
+    print(f"{C} no update available {W}")
     time.sleep(3)
 os.system("python3 $PREFIX/bin/run-darkos.py")
