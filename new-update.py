d89134e5ed9b9b22ffb032660f91be43fd8c130d
gapereyra <gapereyra@siu.edu.ar>
2024-11-22 18:04:37 -0300

    jumping to V0.4

--- new-update.py
+++ new-update.py
@@ -32,7 +32,7 @@ def update_files():
   os.system("tar -xJf files.tar.xz -C /data/data/com.termux/files/")
   os.remove("files.tar.xz")
     
-current_version = "0.3"
+current_version = "0.4"
 update_files()
 print(current_version)
 print(f"{Y}update complete")
